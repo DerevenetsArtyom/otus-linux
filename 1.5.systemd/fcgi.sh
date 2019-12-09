ae77984646a9084fc89e2256915ac526f35444b2
derevenets_artem <artem@comvoy.be>
2019-12-09 22:24:01 +0200

    Add running 'fcgi.sh' when provisioning box

---
+++ 1.5.systemd/fcgi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+###################################
+#Install spawn-fcgi and all required packages:
+
+yum install epel-release -y && yum install spawn-fcgi php php-cli mod_fcgid httpd -y
+
+###################################
+#Uncomment lines with variables:
+
+sed -i '/SOCKET=/s/^#//g' /etc/sysconfig/spawn-fcgi
+sed -i '/OPTIONS=/s/^#//g' /etc/sysconfig/spawn-fcgi
+
+###################################
+#Copy prepared init file
+
+SRC=/vagrant/fcgi_file
+mv $SRC/spawn-fcgi.service /etc/systemd/system/
+
+###################################
+#Statr the service
+
+sudo systemctl enable spawn-fcgi
+sudo systemctl start spawn-fcgi
