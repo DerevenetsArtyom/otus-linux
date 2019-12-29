ae4926129e3947b145ce1214bf41443334d76486
derevenets_artem <artem@comvoy.be>
2019-12-29 03:34:46 +0200

    Add sending email to 'log-analyzer.sh' in '1.6.bash_scripts'

--- 1.6.bash_scripts/log-analyzer.sh
+++ 1.6.bash_scripts/log-analyzer.sh
@@ -43,7 +43,7 @@ function make_report () {
     # Form the body of the mail
     echo "### Report statistics ###" > mail_report
     echo -e "\nLog analize events between $first_event - $last_event" >> mail_report
-    echo -e "\nRequests count by CODE:\n" >> mail_report
+    echo -e "\nRequests count by CODE:" >> mail_report
     
     all_codes=$($check_file | awk '{print $9}' | sort -u | grep -v -)
     for i in $all_codes; do 
@@ -51,24 +51,21 @@ function make_report () {
         echo "    Code $i requests count - $code_count" >> mail_report
     done;
     
-    echo -e "\nTop 10 User IP by accessing server:\n" >> mail_report
+    echo -e "\nTop 10 User IP by accessing server:" >> mail_report
     $check_file | awk '{print "    " $1}' | sort -nr | uniq | head >> mail_report
     echo "" >> mail_report
     
-    #подсчитываем TOP10 запрашиваемых адресов (с наибольшим кол-вом запросов) 
     echo "Top 10 URL Address:" >> mail_report
     $check_file | awk '{print $7}' | sort | uniq -c | sort -nr | head >> mail_report
     echo "" >> mail_report
     
-    #записываем все ошибки c момента последнего запуска
-    echo -e "All errors list:\n" >> mail_report
-    $check_file | awk '($9 !~ /200|301/) && $6 ~ /GET|POST|HEAD/' | awk '{print $1" "$4" "$5" "$6" "$7" "$8" "$9}' >> mail_report
+    echo -e "All errors list:" >> mail_report
+    $check_file | awk '($9 !~ /200|301/) && $6 ~ /GET|POST|HEAD/' | awk '{print "    "$1" "$4" "$5" "$6" "$7" "$8" "$9}' >> mail_report
     echo "" >> mail_report
     
     echo -e "Last parsed line in log - #$last_parsed_line_number" >> mail_report
     echo -e "Line: $last_parsed_string" >> mail_report
     echo -e "\nRun at $(date)" >> mail_report
-
 }
 
 ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -86,11 +83,9 @@ else
         trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT
 
         make_report $1
+        mail -s "Report from nginx server" derevenetsartyom@gmail.com < mail_report
+
         rm -f "$lockfile"
         trap - INT TERM EXIT
     fi
 fi
-
-# echo -e "$report" | mail -s "Report log of [ $date_begin - $date_end ]" root
-# mail -s "Report statistics" "vagrant" < mail_report
-# mail -s "Stat from nginx server $dt" derevenetsartyom@gmail.com < $OUTPUT
